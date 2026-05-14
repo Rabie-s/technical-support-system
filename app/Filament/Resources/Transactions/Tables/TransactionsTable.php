406a82a7ef147e78646ec9e6b5f39f96cbf7a6bc
Rabie-s <asdfw.jsd@gmail.com>
2026-05-14 22:09:11 +0300

    sort by creaed_at

--- app/Filament/Resources/Transactions/Tables/TransactionsTable.php
+++ app/Filament/Resources/Transactions/Tables/TransactionsTable.php
@@ -12,6 +12,7 @@ class TransactionsTable
     public static function configure(Table $table): Table
     {
         return $table
+            ->defaultSort('created_at', 'desc')
             ->columns([
                 TextColumn::make('product.name')
                     ->searchable(),
